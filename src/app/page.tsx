031b7b83deef8f880d81dd6b75ca4e0861f2d0bf
Yuto-Dev0 <yuto.dev0@gmail.com>
2024-04-07 11:46:42 +0900

    Implement logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,149 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import {
+  Blockquote,
+  Box,
+  Button,
+  Card,
+  Dialog,
+  Flex,
+  Heading,
+  Progress,
+  Separator,
+  Slider,
+  Text,
+} from "@radix-ui/themes";
+import InfoBox from "../components/InfoBox.tsx";
+import Image from "next/image.js";
+import React, { useRef } from "react";
+import { useEffect, useState } from "react";
+export default function Main() {
+  const [length, setLength] = useState("0:00");
+  const [permission, setPermission] = useState(false);
+  const [isRecording, setRecordingState] = useState(false);
+  const [audio, setAudio] = useState("");
+  const [audioChunks, setAudioChunks]: [
+    Blob[] | undefined,
+    React.Dispatch<React.SetStateAction<Blob[] | undefined>>
+  ] = useState();
+  const mediaRecorder = useRef<MediaRecorder>();
+  const mediaStream = useRef<MediaStream>();
+  async function getStreamData() {
+    const streamData = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false,
+    });
+    setPermission(true);
+    mediaStream.current = streamData;
+  }
+  useEffect(() => {
+    if ("MediaRecorder" in window) {
+      getStreamData()
+        .then((data) => {
+          if (mediaStream.current) {
+            mediaRecorder.current = new MediaRecorder(mediaStream.current, {
+              mimeType: "audio/webm",
+            });
+          }
+        })
+        .catch(console.error);
+    }
+  }, []);
+
+  function startRecording() {
+    if (mediaRecorder.current && !isRecording) {
+      mediaRecorder.current.start();
+      setRecordingState(true);
+      console.log("Recording Started");
+      let localAudioChunks: Blob[] = [];
+      mediaRecorder.current.ondataavailable = (event) => {
+        if (typeof event.data === "undefined") return;
+        if (event.data.size === 0) return;
+        localAudioChunks.push(event.data);
+      };
+      setAudioChunks(localAudioChunks);
+    }
+  }
+
+  function stopRecording() {
+    if (isRecording && mediaRecorder.current) {
+      setRecordingState(false);
+      mediaRecorder.current.stop();
+      mediaRecorder.current.onstop = () => {
+        setAudio(
+          URL.createObjectURL(new Blob(audioChunks, { type: "audio/webm" }))
+        );
+        setAudioChunks([]);
+        console.log("Recording Stopped");
+      };
+    }
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Flex
+      as="div"
+      direction="column"
+      align="center"
+      justify="center"
+      height="100vh"
+      m="3"
+    >
+      <audio src={audio} autoPlay />
+      <Heading>Echo</Heading>
+      <Heading m="4">-Simplest Voice Training App-</Heading>
+      <Box minWidth="40vw" maxWidth="60vw">
+        <Card size="1">
+          <Flex direction="row" align="center" justify="center" gap="4">
+            <Separator size="4" orientation="vertical" />
+            <Flex
+              align="center"
+              gap="3"
+              direction="column"
+              minWidth="50%"
+              my="4"
+            >
+              {/* Control Buttons */}
+              <Flex gap="2" m="2" justify="center">
+                <Button onClick={startRecording}>Record</Button>
+                <Separator size="2" orientation="vertical" />
+                <Button onClick={stopRecording}>Stop</Button>
+                <Separator size="2" orientation="vertical" />
+                <Button>Play</Button>
+                <Separator size="2" orientation="vertical" />
+                <Dialog.Root>
+                  <Dialog.Trigger>
+                    <Button>Set Loop</Button>
+                  </Dialog.Trigger>
+                  <Dialog.Content>
+                    <Flex direction="column" gap="3">
+                      <label>
+                        <Text></Text>
+                      </label>
+                    </Flex>
+                  </Dialog.Content>
+                </Dialog.Root>
+              </Flex>
+              <Separator size="4" />
+              <Flex
+                direction="row"
+                align="center"
+                justify="center"
+                width="100%"
+                gap="1"
+              >
+                <Box minWidth="60%">
+                  <Progress value={0} size="2" />
+                </Box>
+                <Text size="2">{length}</Text>
+                <Separator orientation="vertical" />
+                <Box minWidth="20%">
+                  <Slider defaultValue={[30]} size="1" />
+                </Box>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Card>
+      </Box>
+    </Flex>
   );
 }
